e8e69643b8168614d2b195e8f25707dadb37c6cf
Sanket2055 <sanketdiwate95@gmail.com>
2023-09-30 19:32:07 +0530

    added get user by id in repo layer

--- src/repository/user-reposirtory.js
+++ src/repository/user-reposirtory.js
@@ -19,6 +19,18 @@ class userRepository {
             throw error;
         }
     }
+
+    async getById(userId) {
+        try {
+            const user = await User.findByPk(userId, {
+                attributes: ['email', 'id']
+            });
+            return user;
+        } catch (error) {
+            console.log("something went wrong: repository Layer", error)
+            throw error;
+        }
+    }
 }
 
 module.exports = userRepository;
