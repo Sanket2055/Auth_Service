4810dd71750b015cde5f577a6117ec3c67135bb7
Sanket2055 <sanketdiwate95@gmail.com>
2023-09-30 23:14:53 +0530

    added validation:

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/user-controller');
-
-router.post("/signup", userController.create);
-router.post("/signin", userController.signIn);
+const { AuthRequestValidator } = require('../../middlewares/index');
+router.post("/signup", AuthRequestValidator.validateUserAuth, userController.create);
+router.post("/signin", AuthRequestValidator.validateUserAuth, userController.signIn);
 
 
 module.exports = router;
